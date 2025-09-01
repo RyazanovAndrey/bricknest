04be05bf35299f070812ef9480d2402a472ef910
RyazanovAndrey <ryazanov.andrej@gmail.com>
2025-09-01 15:18:45 +0300

    end header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Phone, User } from 'lucide-react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router'
+import Modal from './Modal'
 
 const Header = () => {
 
   const location = useLocation().pathname
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen)
+  }
 
   const navLinks = [
     { href: '/', title: 'Home' },
@@ -16,13 +23,24 @@ const Header = () => {
     <header className='w-full bg-header text-white'>
       <div className="container mx-auto px-3 py-3 flex items-center justify-between">
         <div className="text-3xl font-medium">Bric<span className='text-main'>kn</span>est</div>
-        <div className="flex gap-x-16">
+        <div className="flex gap-x-16 items-center">
           {navLinks.map(link => {
             const isActive = location === link.href
             return <Link to={link.href} className={`relative hover:text-main transition-all ${isActive ? 'text-main after:w-full after:h-1 after:bg-main after:-top-1 after:left-0 after:absolute' : ''}`}>{link.title}</Link>
           })}
         </div>
+        <div className="flex items-center gap-x-6">
+          <div className="flex items-center gap-x-2">
+            <Phone />
+            800-500-44-44
+          </div>
+          <button onClick={() => setIsOpen(true)} className='cursor-pointer'>
+            <User />
+          </button>
+          <button className='bg-main py-2 px-4 rounded-md cursor-pointer'>Add listing</button>
+        </div>
       </div>
+      {isOpen && <Modal toggleModal={toggleModal} />}
     </header>
   )
 }
